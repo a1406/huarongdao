b1585a23ea3a6ec9e0bda97b42831e0be7a1a815
jack <dfdf@company.com>
2018-10-27 16:07:35 +0800

    1

--- test.cpp
+++ test.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[])
 {
-    
-    return 0;
+	mymap m, m2;
+	int player[PLAYER_NUM] = {404, 403, 402, 401, 113, 310, 311, 312, 231, 233}; //层层设防
+	int player2[PLAYER_NUM] = {402, 404, 433, 434, 113, 300, 310, 320, 331, 212}; //兵临曹营
+	
+	m.init_map(player);
+	m2.init_map(player2);	
+	return 0;
 }
